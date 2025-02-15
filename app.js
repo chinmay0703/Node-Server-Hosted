082f42da8b5710f116a7823061071f5e26b0f1c8
Chinmay Satish Vyawahare <121102640+chinmay0703@users.noreply.github.com>
2025-02-15 18:16:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
 
 const app = express();
-const port = 3000;
-
-app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+app.get("/api/games", async (req, res) => {
+    try {
+        const response = await fetch("https://www.freetogame.com/api/games");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching games" });
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on Port:${port}`);
+app.get("/api/games/pc", async (req, res) => {
+    try {
+        const response = await fetch("https://www.freetogame.com/api/games?platform=pc");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching games" });
+    }
 });
+app.get("/api/games/browser", async (req, res) => {
+    try {
+        const response = await fetch("https://www.freetogame.com/api/games?platform=browser");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching games" });
+    }
+});
+app.get("/api/games/all", async (req, res) => {
+    try {
+        const response = await fetch("https://www.freetogame.com/api/games?platform=all");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching games" });
+    }
+});
+app.get("/api/games/category/:category", async (req, res) => {
+    try {
+        const category = req.params.category;
+        const response = await fetch(`https://www.freetogame.com/api/games?category=${category}`);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: `Error fetching games in category: ${category}` });
+    }
+});
+
+
+app.listen(5000, () => console.log("Proxy server running on port 5000"));
